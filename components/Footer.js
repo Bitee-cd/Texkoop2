eac74c374293288e750ba7002cf1bd06cb598e94
Bitee-cd <Calebduniya45@gmail.com>
2023-02-01 05:01:59 +0100

    font-footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="bg-pri_dark py-10 text-white ">
       <div className="screen-center ">
-        <div className="grid grid-cols-2 lg:flex lg:w-full justify-between">
+        <div className="hidden lg:flex lg:w-full justify-between">
           <Link href="/about">About</Link>
           <Link href="/rider" className="text-end">
             Become A Rider
@@ -24,6 +24,40 @@ const Footer = () => {
           <Link href="/faqs" className="text-end ">
             FAQs
           </Link>
+        </div>
+        <div className="lg:hidden flex justify-between text-[15px] md:text-[17px]">
+          <div>
+            <div>
+              <Link href="/about">About</Link>
+            </div>
+            <div>
+              <Link href="/privacy-policy" className="">
+                Privacy Policy
+              </Link>
+            </div>
+            <div>
+              <Link href="/terms-and-conditions" className="">
+                Terms and Conditions
+              </Link>
+            </div>
+          </div>
+          <div>
+            <div>
+              <Link href="/rider" className="">
+                Become A Rider
+              </Link>
+            </div>
+            <div>
+              <Link href="/get-started" className="">
+                Partner With Us
+              </Link>
+            </div>
+            <div>
+              <Link href="/faqs" className=" ">
+                FAQs
+              </Link>
+            </div>
+          </div>
         </div>
         <hr className=" bg-white border-white border w-full my-5 lg:my-10 "></hr>
         <div className="flex justify-between">
