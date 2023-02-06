ab82699f60b7f365557ce404bbf09016a2588958
Bitee-cd <Calebduniya45@gmail.com>
2023-02-06 04:34:15 +0100

    input area

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Layout from "../../components/Layout";
+import { useAppContext } from "../../components/AppContext";
 
 const BlogDetail = () => {
-  return <div>BlogDetail</div>;
+  const { setLogoDark } = useAppContext();
+  useEffect(() => {
+    setLogoDark(true);
+  }, []);
+  return (
+    <Layout
+      title="Blog"
+      bgNav="bg-white"
+      textNav="text-bg-pri_dark"
+      bgHamburger="bg-pri_dark"
+    >
+      Blog
+    </Layout>
+  );
 };
 
 export default BlogDetail;
